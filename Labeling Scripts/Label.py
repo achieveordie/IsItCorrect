e209ce6baba9ed97fa3a03efb134f8218889dc4c
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2020-07-15 18:37:02 +0530

    Add Label Defined Type and info

---
+++ Labeling Scripts/Label.py
@@ -0,0 +1,22 @@
+class Label:
+    def __init__(self, correct):
+        self.correct = correct
+        self.given = None
+        self.actual = None
+
+    def assign(self, sentence1, sentence2=None):
+        self.given = sentence1
+        if self.correct:
+            self.actual = sentence1
+        else:
+            self.actual = sentence2
+
+    def __str__(self):
+        return "{" + "correct:{}, ".format(self.correct) + \
+               "given:{}, ".format(self.given) + \
+               "actual:{}".format(self.actual) + "}"
+
+    def __repr__(self):
+        return str("{" + "correct:{}, ".format(self.correct) + \
+                   "given:{}, ".format(self.given) + \
+                   "actual:{}".format(self.actual) + "}")
