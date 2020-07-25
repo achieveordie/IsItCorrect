7abad5372c2cdec0416f80a16063e831ab5af040
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2020-07-25 21:02:35 +0530

    Improve technique, Add more methods to Label.py

--- Labeling Scripts/Label.py
+++ Labeling Scripts/Label.py
@@ -1,5 +1,6 @@
 """Refer to Labeling-Technique.txt for more information"""
 
+
 class Label:
 
     def __init__(self, correct):
@@ -8,11 +9,11 @@ class Label:
         self.actual = None
 
     def assign(self, sentence1, sentence2=None):
-        self.given = sentence1
+        self.actual = sentence1
         if self.correct:
-            self.actual = sentence1
+            self.given = sentence1
         else:
-            self.actual = sentence2
+            self.given = sentence2
 
     def __str__(self):
         return "{" + "correct:{}, ".format(self.correct) + \
@@ -23,3 +24,15 @@ class Label:
         return str("{" + "correct:{}, ".format(self.correct) + \
                    "given:{}, ".format(self.given) + \
                    "actual:{}".format(self.actual) + "}")
+
+    def getList(self):
+        return self.actual.split(" "), self.given.split(" ")
+
+    def difference(self):
+        if self.correct:
+            print("No Difference")
+        else:
+            a, g = self.getList()
+            for i in range(len(a)):
+                if a[i] != g[i]:
+                    print("Difference at ", i+1, "\n 'In actual': ", a[i], ", 'In given': ", g[i])
