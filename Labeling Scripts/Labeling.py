5869469154acb2a143318f2b22b82fbcffec21c6
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2020-07-26 23:28:22 +0530

    Add intensity-1 to the code

--- Labeling Scripts/Labeling.py
+++ Labeling Scripts/Labeling.py
@@ -7,7 +7,7 @@ from Label import Label
 import copy
 
 
-def swap_apart(line, k, int_range):
+def swap_apart(line, k, int_range):     # for intensity_0
     choices = random.choices(int_range, k=k)
     for i in choices:
         if random.randint(0, 1):
@@ -17,13 +17,21 @@ def swap_apart(line, k, int_range):
     return line
 
 
-def swap_adjacent(line, k, int_range):
+def swap_adjacent(line, k, int_range):  # for intensity_0
     choices = random.choices(int_range, k=k)
     for i in choices:
         if random.randint(0, 1):  # if 1 then forward change else backward
             line[i + 1], line[i] = line[i], line[i + 1]
         else:
             line[i - 1], line[i] = line[i], line[i - 1]
+    return line
+
+
+def three_words_swap(line, times): # for intensity_1
+    int_range = [i for i in range(len(line))]
+    for time in range(times):
+        choices = random.choices(int_range, k=3)
+        line[choices[0]], line[choices[1]], line[choices[2]] = line[choices[2]], line[choices[0]], line[choices[1]]
     return line
 
 
@@ -63,8 +71,8 @@ def change_intensity(lines):
 
 def intensity_0(line):
     if line[0]:  # If change value is 1
-        len_sen = len(line[1].split(" "))
         correct_line = line[1].split(" ")
+        len_sen = len(correct_line)
         line = copy.copy(correct_line)
         int_range = [i for i in range(2, len_sen - 2)]
         if random.randint(0, 1):  # choose adjacent if 1 or one word apart
@@ -86,14 +94,40 @@ def intensity_0(line):
             else:
                 changed = swap_apart(line, 1, int_range)
         label = Label(False)
-        label.assign(correct_line, changed)
+        label.assign(" ".join(correct_line), " ".join(changed))
         return label
 
 
 def intensity_1(line):
     if line[0]:
-        print(line[1])
-    return line
+        correct_line = line[1].split(" ")[1:-1]  # remove <START> and <END>
+        len_sen = len(correct_line)
+        line = copy.copy(correct_line)
+        correct_line.insert(0, '<START>')
+        correct_line.append('<END>')
+        if len_sen > 40:
+            changed = three_words_swap(line[:len_sen // 2], 2)
+            changed.append(three_words_swap(line[len_sen // 2, :], 2))
+        elif 40 >= len_sen > 20:
+            changed = three_words_swap(line[:len_sen // 3], 1)
+            changed.append(three_words_swap(line[len_sen // 3:2 * len_sen // 3], 1))
+            changed.append(three_words_swap(line[2 * len_sen // 3:], 1))
+        elif 20 >= len_sen > 10:
+            changed = three_words_swap(line[:len_sen // 2], 1)
+            changed.append(three_words_swap(line[len_sen // 2:], 1))
+        else:
+            changed = three_words_swap(line, 1)
+        changed.insert(0, '<START>')
+        changed.append('<END>')
+        answer = ""
+        for value in changed:
+            try:
+                answer = answer + " " + value
+            except:
+                answer = answer + " " + " ".join(value)
+        label = Label(False)
+        label.assign(" ".join(correct_line), answer)
+        return label
 
 
 def intensity_2(line):
