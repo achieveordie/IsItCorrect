1441ec77e95c66e2c72ef004f381c0c59670de25
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2020-08-15 22:01:52 +0530

    Modify code to train on GPU

--- ML/Beta/labeling.py
+++ ML/Beta/labeling.py
@@ -202,7 +202,7 @@ def store_label1(lines):
 
 if __name__ == '__main__':
     pattern = re.compile("<START>.*?<END>")
-    with closing(open(r"D:\Datasets\IsItCorrect\testing-small.txt", 'r', encoding='utf-8')) as file:
+    with closing(open(r"D:\Datasets\IsItCorrect\testing-new.txt", 'r', encoding='utf-8')) as file:
         lines = file.readlines()
         lines = pattern.findall(lines[0])
         print("Total number of lines, ", len(lines))
@@ -218,11 +218,16 @@ if __name__ == '__main__':
         #     print("label_0 {}:".format(i), label_0[i])
         #     print("label_1 {}:".format(i), label_1[i])
         db_train = {}
-        with closing(open(Path(r'D:/Datasets/IsItCorrect/beta_sample_train_small.pkl'),
+        with closing(open(Path(r'D:/Datasets/IsItCorrect/beta_sample_train.pkl'),
                           'ab')) as file:
-            for i, line in enumerate(label_0 + label_1):
-                db_train[i] = line.store_dict()
+            assert len(label_0) == len(label_1)  # should have equal length
+            for i in range(0, len(label_0), 2):
+                db_train[i] = label_0[i].store_dict()
+                db_train[i + 1] = label_1[i].store_dict()
+
             pickle.dump(db_train, file)
 
-
-
+            for i in range(0, len(db_train)-1, 2):
+                print(db_train[i])
+                print(db_train[i+1])
+                print("\n\n")
--- ML/Beta/train.py
+++ ML/Beta/train.py
@@ -1,9 +1,11 @@
 import torch
+import time
 from beta_loader import get_pickle_location, get_data
 from custom_model import getCustomModel
 from transformers import CamembertTokenizer
 
-model = getCustomModel()
+cuda0 = torch.device('cuda:0')
+model = getCustomModel().to(cuda0)
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-05)
 
@@ -23,20 +25,24 @@ def train(epoch, x, actual):
 
 
 def epochs(num_epochs, trainloader):
+    time_start = time.time()
     for epoch in range(num_epochs):
         iter_trainloader = iter(trainloader)
         counter = 0
         for data in iter_trainloader:
             data["sentence"] = tokenizer(data["sentence"], padding=True)
             data["sentence"]["input_ids"] = torch.tensor(data["sentence"]["input_ids"],
-                                                         dtype=torch.long)
-            data["sentence"]["attention_mask"] = torch.tensor(data["sentence"]["attention_mask"])
-            data["label"] = torch.LongTensor(data["label"])
+                                                         dtype=torch.long, device=cuda0)
+            data["sentence"]["attention_mask"] = torch.tensor(data["sentence"]["attention_mask"],
+                                                              device=cuda0)
+            data["label"] = torch.tensor(data["label"], device=cuda0)
             loss = train(epoch, data["sentence"], data["label"])
             counter += 1
-            if True :#counter % 10 == 0:
+            if counter % 10 == 0:
                 print(loss)
         print("Done for Epoch number {}".format(epoch))
+    time_end = time.time()
+    print("Total Time Taken: ", time_end-time_start)
 
 
 if __name__ == '__main__':
@@ -44,4 +50,4 @@ if __name__ == '__main__':
     tokenizer = CamembertTokenizer.from_pretrained(tokenizer_location)
     pickle_location = get_pickle_location()
     trainloader = get_data(pickle_location)
-    epochs(200, trainloader)
+    epochs(50, trainloader)
