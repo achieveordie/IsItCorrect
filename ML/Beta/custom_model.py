358cafa2e7d7a8cf412bf19d894c96e5acea74f7
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2020-09-09 15:11:56 +0530

    reconfigure architecture

--- ML/Beta/custom_model.py
+++ ML/Beta/custom_model.py
@@ -6,8 +6,12 @@ class Camembert(torch.nn.Module):
     def __init__(self, cam_model):
         super(Camembert, self).__init__()
         self.l1 = cam_model
-        for param in cam_model.parameters():
-            param.requires_grad = False
+        total = 199
+        for i, param in enumerate(cam_model.parameters()):
+            if total - i > 5:
+                param.requires_grad = False
+            else:
+                pass
         self.l2 = torch.nn.Dropout(0.1)
         self.l3 = torch.nn.Linear(768, 576)
         self.l4 = torch.nn.Dropout(0.2)
@@ -19,10 +23,29 @@ class Camembert(torch.nn.Module):
         output = self.l2(output)
         output = self.l3(output)
         output = self.l4(output)
+        output = self.l5(output)
+        output = self.l6(output)
         return output
+
+    def printit(self):
+        all_layers = [self.l1, self.l2, self.l3, self.l4, self.l5, self.l6]
+        for layer in all_layers:
+            total = 0
+            for param in layer.parameters():
+                try:
+                    print(param.requires_grad, '\t', param.size())
+                    total += 1
+                except:
+                    print("Not printable")
+            print("Total is {}".format(total))
 
 
 def getCustomModel():
     cam_model = loadInitialModel()
     model = Camembert(cam_model)
     return model
+
+
+# if __name__ == '__main__':
+#     model = getCustomModel()
+#     model.printit()
