2c90beccf4757a873e1ed499d5b1be0c2916dcbb
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2020-10-27 18:22:25 +0530

    add seperate file for hyperparamters, bpe mode

---
+++ ML/Beta-v0.2/beta02_hyperparameters.py
@@ -0,0 +1,19 @@
+"""
+This contains a method which returns all the hyperparameters required for
+training/testing purpose. Value only needs to be changed here.
+"""
+
+
+def get_hps():
+    """
+    `learning_rate`, `batch_size`, `dropout_rate`: self-explainable.
+    `retrain_layers`: Number of last layers from transformer to retrain.
+    :return:
+    """
+    hyperparameters = {
+        "learning_rate": 1e-05,
+        "batch_size": 16,
+        "retrain_layers": 15,
+        "dropout_rate": 0.1
+    }
+    return hyperparameters
