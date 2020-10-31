b1b8ff3d0dcc64deb3a3b882394b27ccc6773dbb
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2020-10-31 14:34:27 +0530

    Hyperparameter tuning

--- ML/Beta-v0.2/beta02_hyperparameters.py
+++ ML/Beta-v0.2/beta02_hyperparameters.py
@@ -8,13 +8,50 @@ def get_hps():
     """
     `learning_rate`, `batch_size`, `dropout_rate`: self-explainable.
     `retrain_layers`: Number of last layers from transformer to retrain.
-    :return:
+    :return: <dict> of hyperparameters.
+
+    The following are the result from training on sample data, useful to decide hps.
+    (loss: 0.1786, time: 74.71) -> hyperparameters = {
+        "learning_rate": 1e-05,
+        "batch_size": 2,
+        "retrain_layers": 15,
+        "dropout_rate": 0.1,
+        "epochs": 10
+    }
+    (loss: 0.1445, time: 86.09 sec) -> hyperparameters = {
+        "learning_rate": 1e-05,
+        "batch_size": 4,
+        "retrain_layers": 15,
+        "dropout_rate": 0.1,
+        "epochs": 20
+    }
+    (loss: 0.1588, time: 87.41) -> hyperparameters = {
+        "learning_rate": 1e-05,
+        "batch_size": 2,
+        "retrain_layers": 30,
+        "dropout_rate": 0.1,
+        "epochs": 10
+    }
+    (loss: 0.1554, time:98.72) -> hyperparameters = {
+        "learning_rate": 1e-05,
+        "batch_size": 2,
+        "retrain_layers": 60,
+        "dropout_rate": 0.1,
+        "epochs": 10
+    }
+    (loss: 0.0527, time: 294.80) -> hyperparameters = {
+        "learning_rate": 1e-05,
+        "batch_size": 2,
+        "retrain_layers": 15,
+        "dropout_rate": 0.1,
+        "epochs": 40
+    }
     """
     hyperparameters = {
         "learning_rate": 1e-05,
         "batch_size": 2,
         "retrain_layers": 15,
         "dropout_rate": 0.1,
-        "epochs": 10
+        "epochs": 40
     }
     return hyperparameters
